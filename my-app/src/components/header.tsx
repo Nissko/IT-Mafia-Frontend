d179babc76a4f9b787cf02e818d67a47336a8814
rmyuk4 <89967350+rmyuk4@users.noreply.github.com>
2023-07-05 19:03:48 +0500

    v1

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -1,1 +1,16 @@
-//lf lflflf
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return <div className="header">
+      <span className="page-title">Семьи</span>
+      
+        <div className="search-bar">
+            <input type="text" name="search" placeholder="Введите запрос" />
+            <button type="submit">Поиск</button>
+        </div>
+      
+    </div>
+}
+
+export default Header;
