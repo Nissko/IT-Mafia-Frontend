fc3b393e50b54071e1e5307e60f9b8dc6e81373e
Georgij Kravtsov <reasgicc@gmail.com>
2023-07-10 20:03:38 +0500

    test

---
+++ my-app/src/components/battle/BattleGame.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BattleGame.css';
+
+const BattleGame = () => {
+  return <div className='BattleGame'>
+          <ul className="list">
+        <li className="item">1</li>
+        <li className="item">2</li>
+        <li className="item">3</li>
+        <li className="item">4</li>
+        <li className="item">5</li>
+        <li className="item">6</li>
+        <li className="item">7</li>
+      </ul>
+  </div>
+}
+
+export default BattleGame;
